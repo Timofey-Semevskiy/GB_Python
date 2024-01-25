e3e307bf1fc8ff1b930bbf739aeac88a068d80a9
Timofey-Semevskiy <vukoko@mail.ru>
2024-01-26 02:42:41 +0300

    3_lesson_2_task

---
+++ 3_lesson_2_task.py
@@ -0,0 +1,11 @@
+def passport():
+    name = input("Enter Name: ")
+    secondName = input("Enter SecondName: ")
+    hometown = input("input hometown: ")
+    phone = int(input("input phone: "))
+    email = input("inpu email: ")
+    print(f"{name} {secondName} {hometown} {phone} {email}")
+
+
+
+passport()
