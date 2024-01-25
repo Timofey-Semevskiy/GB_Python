67ff353c941c9b59e4a6c138497f459b06d7e18c
Timofey-Semevskiy <vukoko@mail.ru>
2024-01-25 18:37:03 +0300

    3_lesson_1_task

---
+++ 3_lesson_1_task.py
@@ -0,0 +1,10 @@
+def subtrack(v1,v2):
+    if v1 and v2 != 0:
+        print(v1/v2)
+    else:
+        print("Zero")
+    
+
+v1 = int(input("Enter first num: "))
+v2 = int(input("Enter second num: "))
+subtrack(v1,v2)
